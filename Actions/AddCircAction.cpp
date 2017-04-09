dc9937052bda42e6633c2aea8f784a0cdcf6c4f5
Mahmoud Adas <mido3ds@gmail.com>
2017-04-09 17:25:35 +0200

    added circ, trngl, not tested

---
+++ Actions/AddCircAction.cpp
@@ -0,0 +1,40 @@
+#include "AddCircAction.h"
+
+AddCircAction::AddCircAction(ApplicationManager* app_p)
+    : Action(app_p)
+{}
+
+void AddCircAction::ReadActionParameters()
+{
+    //Get a Pointer to the Input / Output Interfaces
+    Output* out_p = manager_p->GetOutput();
+    Input* in_p = manager_p->GetInput();
+
+    out_p->PrintMessage("New Circle: Click at first point");
+
+    circ = new CLine();
+
+    //Read 1st corner and store in point p1
+    in_p->GetPointClicked(circ->p1.x, circ->p1.y);
+
+    out_p->PrintMessage("New Circle: Click at second point");
+
+    //Read 2nd corner and store in point p2
+    in_p->GetPointClicked(circ->p2.x, circ->p2.y);
+
+    circ->is_filled = false; //default is not filled
+    //get drawing, filling colors and pen width from the interface
+    circ->draw_clr = out_p->GetCrntDrawColor();
+    circ->fill_clr = out_p->GetCrntFillColor();
+    circ->border_width = out_p->GetCrntPenWidth();
+
+    out_p->ClearStatusBar();
+}
+
+//Execute the action
+void AddCircAction::Execute()
+{
+    ReadActionParameters();
+
+    manager_p->AddFigure(circ);
+}
